b8dec0ff0c813a1b2ca91b31fc193f5d333ac366
Sougata2006 <psougata32@gmail.com>
2026-08-11 18:15:50 +0530

    feat: add initial hello endpoint

---
+++ src/main/java/com/sougata/ecommerce/App.java
@@ -0,0 +1,12 @@
+package com.sougata.ecommerce;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class App {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello E-commerce";
+    }
+}
