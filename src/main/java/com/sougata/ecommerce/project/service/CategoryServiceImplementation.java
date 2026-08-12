c6faaa11245a14cdaa603cbb5ffa67314f18fc7f
Sougata2006 <psougata32@gmail.com>
2026-08-12 21:57:14 +0530

    feat: integrate category APIs with JPA

--- src/main/java/com/sougata/ecommerce/project/service/CategoryServiceImplementation.java
+++ src/main/java/com/sougata/ecommerce/project/service/CategoryServiceImplementation.java
@@ -1,41 +1,49 @@
 package com.sougata.ecommerce.project.service;
 
 import com.sougata.ecommerce.project.model.Category;
+import com.sougata.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CategoryServiceImplementation implements CategoryService {
-    private List<Category> categories = new ArrayList<>();
-    private long nextId = 1;
+//    private List<Category> categories = new ArrayList<>();  //As we have shifted to JpaRepo we do not need list anymore
+//    private long nextId = 1; //As we have shifted to JpaRepo we do not need list count anymore
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getALLCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+//        category.setCategoryId(nextId++); //As we have shifted to JpaRepo we do not need list increment anymore
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
+        List<Category> categories = categoryRepository.findAll();
+
         Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found !!"));
 
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "Category with category id " + categoryId + " deleted successfully !!";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
+
+        List<Category> categories = categoryRepository.findAll();
 
         Optional<Category> optionalCategory = categories.stream().filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst();
@@ -43,7 +51,8 @@ public class CategoryServiceImplementation implements CategoryService {
         if (optionalCategory.isPresent()) {
             Category existingCategory = optionalCategory.get();
             existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
+            Category savedCategory = categoryRepository.save(existingCategory);
+            return savedCategory;
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found !!");
         }
