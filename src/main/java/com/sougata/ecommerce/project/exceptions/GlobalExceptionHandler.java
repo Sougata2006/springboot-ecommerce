8db95e041104dd5267c451731f611ff5e198aa92
Sougata2006 <psougata32@gmail.com>
2026-08-14 01:47:53 +0530

    feat: add category field validation

---
+++ src/main/java/com/sougata/ecommerce/project/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.sougata.ecommerce.project.exceptions;
+
+public class GlobalExceptionHandler {
+
+}
--- src/main/java/com/sougata/ecommerce/project/model/Category.java
+++ src/main/java/com/sougata/ecommerce/project/model/Category.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,15 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
+//(Note) - We can also use 'record' in class to avoid lambok dependency
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
+    @NotBlank
     private String categoryName;
 
 }
