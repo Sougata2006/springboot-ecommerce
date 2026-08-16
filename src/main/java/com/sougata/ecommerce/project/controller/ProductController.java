1b7d4d4463e20375c70555d8d15abdcabc70322a
Sougata2006 <psougata32@gmail.com>
2026-08-16 22:11:08 +0530

    feat: add required annotations

--- src/main/java/com/sougata/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/sougata/ecommerce/project/controller/ProductController.java
@@ -1,4 +1,9 @@
 package com.sougata.ecommerce.project.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class ProductController {
 }
--- src/main/java/com/sougata/ecommerce/project/service/ProductServiceImplementation.java
+++ src/main/java/com/sougata/ecommerce/project/service/ProductServiceImplementation.java
@@ -1,4 +1,7 @@
 package com.sougata.ecommerce.project.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductServiceImplementation implements ProductService{
 }
