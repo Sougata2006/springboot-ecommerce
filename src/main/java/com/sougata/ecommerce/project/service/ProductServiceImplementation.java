7d31460fe52631d38a50d021105c800757a94f24
Sougata2006 <psougata32@gmail.com>
2026-08-17 22:07:12 +0530

    feat: implement product retrival by name/keyword

--- src/main/java/com/sougata/ecommerce/project/service/ProductServiceImplementation.java
+++ src/main/java/com/sougata/ecommerce/project/service/ProductServiceImplementation.java
@@ -69,4 +69,17 @@ public class ProductServiceImplementation implements ProductService{
 
         return productResponse;
     }
+
+    @Override
+    public ProductResponse searchProductByKeyword(String keyword) {
+
+        List<Product> products = productRepository.findByProductNameLikeIgnoreCase('%'+keyword+'%'); //'%' IS ESSENTIAL FOR PATTERN MATCHING FOR KEYWORD
+        List<ProductDTO> productDTOS = products.stream().map(product
+                -> modelMapper.map(product, ProductDTO.class)).toList();
+
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productDTOS);
+
+        return productResponse;
+    }
 }
