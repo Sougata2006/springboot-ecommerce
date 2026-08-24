d50414699616406d3d2ae27296c69e98d528900e
Sougata2006 <psougata32@gmail.com>
2026-08-24 11:19:57 +0530

    feat: configure user entity relationships with JPA

--- src/main/java/com/sougata/ecommerce/project/model/User.java
+++ src/main/java/com/sougata/ecommerce/project/model/User.java
@@ -5,31 +5,43 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.validator.constraints.UniqueElements;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     @NotBlank
     @Size(max = 20)
+    @Column(name = "username")
     private String userName;
 
     @NotBlank
     @Size(max = 25)
     @Email
     @UniqueElements
+    @Column(name = "email")
     private String email;
 
     @NotBlank
     @Size(min = 5, max = 50)
+    @Column(name = "password")
     private String password;
 
 
@@ -38,4 +50,10 @@ public class User {
         this.password = password;
         this.userName = userName;
     }
+
+    @Getter
+    @Setter
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_table", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
