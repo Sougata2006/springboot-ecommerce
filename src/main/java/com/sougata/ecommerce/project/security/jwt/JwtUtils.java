3770d690f10a6f2974f6d1951a0d850d0d5ba964
Sougata <psougata32@gmail.com>
2026-09-26 00:01:32 +0530

    big potato stuffs

--- src/main/java/com/sougata/ecommerce/project/security/jwt/JwtUtils.java
+++ src/main/java/com/sougata/ecommerce/project/security/jwt/JwtUtils.java
@@ -1,7 +1,6 @@
 package com.sougata.ecommerce.project.security.jwt;
 
 import com.sougata.ecommerce.project.security.services.UserDetailsImplementation;
-import com.sougata.ecommerce.project.security.services.UserDetailsServiceImplementation;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
@@ -35,16 +34,16 @@ public class JwtUtils {
     @Value("${spring.app.jwtCookie}")
     private String jwtCookie;
 
-//    public String getJwtFromHeader(HttpServletRequest request) {
-//
-//        String bearerToken = request.getHeader("Authorization");
-//
-//        logger.debug("Authorization Header : {}", bearerToken);
-//        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-//            return bearerToken.substring(7);
-//        }
-//        return null;
-//    }
+    public String getJwtFromHeader(HttpServletRequest request) {
+
+        String bearerToken = request.getHeader("Authorization");
+
+        logger.debug("Authorization Header : {}", bearerToken);
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 
     public String getJwtFromCookies(HttpServletRequest request){
         Cookie cookie = WebUtils.getCookie(request, jwtCookie);
--- src/main/java/com/sougata/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/sougata/ecommerce/project/service/CategoryService.java
@@ -1,6 +1,5 @@
 package com.sougata.ecommerce.project.service;
 
-import com.sougata.ecommerce.project.model.Category;
 import com.sougata.ecommerce.project.payload.CategoryDTO;
 import com.sougata.ecommerce.project.payload.CategoryResponse;
 
